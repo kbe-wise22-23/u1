2bd9e6588185f8a5e85fd6113274f056c5786bf7
Lucas Garbe <hi@lucasgarbe.de>
2022-10-19 16:01:02 +0200

    initial commit clean spring boot starter

---
+++ src/main/java/com/kbe/u1/U1Application.java
@@ -0,0 +1,13 @@
+package com.kbe.u1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbe/u1/U1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbe.u1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
